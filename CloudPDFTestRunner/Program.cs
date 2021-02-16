9293902992b88347b29a91b27260811c3dbfb55e
Adam Diament <adam@clevertree.io>
2021-02-16 21:18:55 +0200

    Example usage

--- CloudPDFTestRunner/Program.cs
+++ CloudPDFTestRunner/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using CloudPDF.NET;
 
 namespace CloudPDFTestRunner
 {
   class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-      var token = CloudPdf.GetViewingToken(
-        "",
-        "",
+      var cloudPDF = new CloudPdf("your-cloud-name", "your-access-secret");
+
+      var token = cloudPDF.GetViewingToken(
         "",
         new DateTime(2021, 12, 09),
         true, true);
 
-      Console.WriteLine(token);
+      var fileName = @"file-location-.pdf"; 
+
+      await using FileStream fs = File.OpenRead(fileName); // the user would get the stream from somewhere, maybe the cloud or his computer
+      
+
+      var response = await cloudPDF.UploadDocument(fs, "test.pdf", DateTime.UtcNow.AddYears(1), true, true);
+      
+      Console.WriteLine(response.Upload.Id);
     }
   }
 }
