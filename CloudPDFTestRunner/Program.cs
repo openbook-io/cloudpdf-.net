5001f0a81f38b450bb9c8587dd1a3eebcf5040cd
Adam Diament <adam@clevertree.io>
2021-02-22 09:57:16 +0200

    Add test data to test program

--- CloudPDFTestRunner/Program.cs
+++ CloudPDFTestRunner/Program.cs
@@ -9,20 +9,20 @@ namespace CloudPDFTestRunner
   {
     static async Task Main(string[] args)
     {
-      var cloudPDF = new CloudPdf("your-cloud-name", "your-access-secret");
+      var cloudPDF = new CloudPdf("You-Cloud-Name", "Your-Access-Secret");
 
       var token = cloudPDF.GetViewingToken(
-        "",
+        "Your-Document-Id",
         new DateTime(2021, 12, 09),
         true, true);
       
       Console.WriteLine(token.Payload);
 
-      var fileName = @"file-location-.pdf"; 
+      var fileName = @"Your-File-Location.pdf"; 
 
-      using FileStream fs =  File.OpenRead(fileName); // the user would get the stream from somewhere, maybe the cloud or his computer
+      await using FileStream fs =  File.OpenRead(fileName); // the user would get the stream from somewhere, maybe the cloud or his computer
       
-      var response = await cloudPDF.UploadDocument(fs, "test.pdf", DateTime.UtcNow.AddYears(1), true, true);
+      var response = await cloudPDF.UploadDocument(fs, "Your-File-Name.pdf", DateTime.UtcNow.AddYears(1), true, true);
       
       Console.WriteLine(response.Payload.Id);
     }
